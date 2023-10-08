62fd89153797e54628a88adf53723d50ce9ced2c
Xithrius <xithrius@gmail.com>
2023-10-07 21:35:23 -0700

    Migrated API from template structure to my own

--- api/app/main.py
+++ api/app/main.py
@@ -1,107 +1,105 @@
-"""Basic FastAPI app for CRUD operations on users and posts using Prisma Client Python"""
-from typing import Optional, List
-
 from fastapi import FastAPI
-from prisma import Prisma
-from prisma.models import User, Post
-from prisma.types import UserUpdateInput
-from prisma.partials import UserWithoutRelations, PostWithoutRelations
 
+from prisma import Prisma
+from prisma.models import Post, User
+from prisma.partials import PostWithoutRelations, UserWithoutRelations
+from prisma.types import UserUpdateInput
 
 app = FastAPI()
 prisma = Prisma(auto_register=True)
 
 
-@app.on_event('startup')  # type: ignore
+@app.on_event("startup")
 async def startup() -> None:
-    await prisma.connect()
+    if not prisma.is_connected():
+        await prisma.connect()
 
 
-@app.on_event('shutdown')  # type: ignore
+@app.on_event("shutdown")
 async def shutdown() -> None:
     if prisma.is_connected():
         await prisma.disconnect()
 
 
 @app.get(
-    '/users',
-    response_model=List[UserWithoutRelations],
+    "/users",
+    response_model=list[UserWithoutRelations],
 )
-async def list_users(take: int = 10) -> List[User]:
+async def list_users(take: int = 10) -> list[User]:
     return await User.prisma().find_many(take=take)
 
 
 @app.post(
-    '/users',
+    "/users",
     response_model=UserWithoutRelations,
 )
-async def create_user(name: str, email: Optional[str] = None) -> User:
-    return await User.prisma().create({'name': name, 'email': email})
+async def create_user(name: str, email: str | None = None) -> User:
+    return await User.prisma().create({"name": name, "email": email})
 
 
 @app.put(
-    '/users/{user_id}',
+    "/users/{user_id}",
     response_model=UserWithoutRelations,
 )
 async def update_user(
     user_id: str,
-    name: Optional[str] = None,
-    email: Optional[str] = None,
-) -> Optional[User]:
+    name: str | None = None,
+    email: str | None = None,
+) -> User | None:
     data: UserUpdateInput = {}
 
     if name is not None:
-        data['name'] = name
+        data["name"] = name
 
     if email is not None:
-        data['email'] = email
+        data["email"] = email
 
     return await User.prisma().update(
         where={
-            'id': user_id,
+            "id": user_id,
         },
         data=data,
     )
 
 
 @app.delete(
-    '/users/{user_id}',
+    "/users/{user_id}",
     response_model=User,
 )
-async def delete_user(user_id: str) -> Optional[User]:
+async def delete_user(user_id: str) -> User | None:
     return await User.prisma().delete(
         where={
-            'id': user_id,
+            "id": user_id,
         },
         include={
-            'posts': True,
+            "posts": True,
         },
     )
 
 
 @app.get(
-    '/users/{user_id}',
+    "/users/{user_id}",
     response_model=UserWithoutRelations,
 )
-async def get_user(user_id: str) -> Optional[User]:
+async def get_user(user_id: str) -> User | None:
     return await User.prisma().find_unique(
         where={
-            'id': user_id,
+            "id": user_id,
         },
     )
 
 
 @app.get(
-    '/users/{user_id}/posts',
-    response_model=List[PostWithoutRelations],
+    "/users/{user_id}/posts",
+    response_model=list[PostWithoutRelations],
 )
-async def get_user_posts(user_id: str) -> List[Post]:
+async def get_user_posts(user_id: str) -> list[Post]:
     user = await User.prisma().find_unique(
         where={
-            'id': user_id,
+            "id": user_id,
         },
         include={
-            'posts': True,
+            "posts": True,
         },
     )
     if user is not None:
@@ -112,17 +110,17 @@ async def get_user_posts(user_id: str) -> List[Post]:
 
 
 @app.post(
-    '/users/{user_id}/posts',
+    "/users/{user_id}/posts",
     response_model=PostWithoutRelations,
 )
 async def create_post(user_id: str, title: str, published: bool) -> Post:
     return await Post.prisma().create(
         data={
-            'title': title,
-            'published': published,
-            'author': {
-                'connect': {
-                    'id': user_id,
+            "title": title,
+            "published": published,
+            "author": {
+                "connect": {
+                    "id": user_id,
                 },
             },
         }
@@ -130,38 +128,38 @@ async def create_post(user_id: str, title: str, published: bool) -> Post:
 
 
 @app.get(
-    '/posts',
-    response_model=List[PostWithoutRelations],
+    "/posts",
+    response_model=list[PostWithoutRelations],
 )
-async def list_posts(take: int = 10) -> List[Post]:
+async def list_posts(take: int = 10) -> list[Post]:
     return await Post.prisma().find_many(take=take)
 
 
 @app.get(
-    '/posts/{post_id}',
+    "/posts/{post_id}",
     response_model=Post,
 )
-async def get_post(post_id: str) -> Optional[Post]:
+async def get_post(post_id: str) -> Post | None:
     return await Post.prisma().find_unique(
         where={
-            'id': post_id,
+            "id": post_id,
         },
         include={
-            'author': True,
+            "author": True,
         },
     )
 
 
 @app.delete(
-    '/posts/{post_id}',
+    "/posts/{post_id}",
     response_model=Post,
 )
-async def delete_post(post_id: str) -> Optional[Post]:
+async def delete_post(post_id: str) -> Post | None:
     return await Post.prisma().delete(
         where={
-            'id': post_id,
+            "id": post_id,
         },
         include={
-            'author': True,
+            "author": True,
         },
     )
--- api/prisma/partial_types.py
+++ api/prisma/partial_types.py
@@ -1,4 +1,10 @@
-from prisma.models import User, Post
+from prisma.models import Post, User
 
-User.create_partial('UserWithoutRelations', exclude_relational_fields=True)
-Post.create_partial('PostWithoutRelations', exclude_relational_fields=True)
+User.create_partial(
+    "UserWithoutRelations",
+    exclude_relational_fields=True,
+)
+Post.create_partial(
+    "PostWithoutRelations",
+    exclude_relational_fields=True,
+)
